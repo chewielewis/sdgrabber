e5a23a7d831b74234fb6252713a15b30908b7f8f
chewielewis <timandrewellis@gmail.com>
2025-06-18 00:57:47 +1200

    trying fix

--- mount-sdcard.sh
+++ mount-sdcard.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# wait 1 second to ensure the device is ready
-sleep 1
-
 # Get the device name passed from udev
 DEVICE="/dev/$1"
 
@@ -17,16 +14,38 @@ log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
 }
 
+# Debug info
+log_message "Device detected: $DEVICE"
+log_message "Running mount script as user: $(whoami)"
+
 # Make sure mount point exists
 if [ ! -d "$MOUNT_POINT" ]; then
-    mkdir -p "$MOUNT_POINT"
+    sudo mkdir -p "$MOUNT_POINT"
+    log_message "Created mount point: $MOUNT_POINT"
 fi
 
-# Mount the device
-sudo mount "$DEVICE" "$MOUNT_POINT"
+# Get filesystem type
+FS_TYPE=$(sudo blkid -s TYPE -o value "$DEVICE")
+log_message "Filesystem type detected: $FS_TYPE"
+
+# Mount the device with appropriate options based on filesystem
+case $FS_TYPE in
+    vfat|fat32)
+        sudo mount -t vfat -o uid=pi,gid=pi,umask=000 "$DEVICE" "$MOUNT_POINT"
+        ;;
+    exfat)
+        sudo mount -t exfat "$DEVICE" "$MOUNT_POINT"
+        ;;
+    ntfs)
+        sudo mount -t ntfs "$DEVICE" "$MOUNT_POINT"
+        ;;
+    *)
+        sudo mount "$DEVICE" "$MOUNT_POINT"
+        ;;
+esac
 
 if [ $? -eq 0 ]; then
-    log_message "Successfully mounted SD card at $MOUNT_POINT"
+    log_message "Successfully mounted $DEVICE at $MOUNT_POINT"
 else
-    log_message "Failed to mount SD card from $DEVICE"
+    log_message "Failed to mount $DEVICE. Exit code: $?"
 fi
