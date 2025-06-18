bdb6be8d1c440b47810d1a9f7a44726b8e074801
chewielewis <timandrewellis@gmail.com>
2025-06-19 01:04:41 +1200

    new unmount service

--- unmount-sdcard.sh
+++ unmount-sdcard.sh
@@ -2,8 +2,6 @@
 
 # Set full paths
 UMOUNT="/bin/umount"
-
-# Mount point
 MOUNT_POINT="/mnt/sdcard"
 
 # Log with timestamp
@@ -11,17 +9,21 @@ log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> /var/log/sdgrabber.log
 }
 
-log_message "Starting unmount script"
+log_message "Starting unmount process"
 
-# Check if mounted and unmount
+# Force unmount if device is busy
 if mountpoint -q "$MOUNT_POINT"; then
     log_message "Unmounting device from $MOUNT_POINT"
-    $UMOUNT "$MOUNT_POINT"
+    $UMOUNT -f "$MOUNT_POINT"
     if [ $? -eq 0 ]; then
         log_message "Successfully unmounted device"
     else
-        log_message "Failed to unmount device"
+        log_message "Failed to unmount device, trying lazy unmount"
+        $UMOUNT -l "$MOUNT_POINT"
+        if [ $? -eq 0 ]; then
+            log_message "Lazy unmount successful"
+        else
+            log_message "All unmount attempts failed"
+        fi
     fi
-else
-    log_message "No device mounted at $MOUNT_POINT"
 fi
