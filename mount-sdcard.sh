94a3c3fd52a63e0cdbc8eb62142d4dace9fc0602
chewielewis <timandrewellis@gmail.com>
2025-06-19 00:22:14 +1200

    fixed permissions for sdgrabber rules and mount script

--- mount-sdcard.sh
+++ mount-sdcard.sh
@@ -19,15 +19,16 @@ log_message() {
 
 # Debug info
 log_message "Starting mount script for device: $DEVICE"
-log_message "Environment: $(env)"
 
 # Sleep briefly to ensure device is ready
 sleep 2
 
-# Make sure mount point exists
+# Make sure mount point exists with correct permissions
 if [ ! -d "$MOUNT_POINT" ]; then
     /usr/bin/mkdir -p "$MOUNT_POINT"
-    log_message "Created mount point: $MOUNT_POINT"
+    /usr/bin/chown root:root "$MOUNT_POINT"
+    /usr/bin/chmod 755 "$MOUNT_POINT"
+    log_message "Created mount point: $MOUNT_POINT with permissions"
 fi
 
 # Get filesystem type
@@ -37,22 +38,24 @@ log_message "Filesystem type detected: $FS_TYPE"
 # Mount the device with appropriate options based on filesystem
 case $FS_TYPE in
     vfat|fat32)
-        /usr/bin/mount -t vfat -o uid=1000,gid=1000,umask=000 "$DEVICE" "$MOUNT_POINT"
+        /bin/mount -t vfat -o rw,uid=1000,gid=1000,umask=000 "$DEVICE" "$MOUNT_POINT"
         ;;
     exfat)
-        /usr/bin/mount -t exfat "$DEVICE" "$MOUNT_POINT"
+        /bin/mount -t exfat -o rw,uid=1000,gid=1000 "$DEVICE" "$MOUNT_POINT"
         ;;
     ntfs)
-        /usr/bin/mount -t ntfs "$DEVICE" "$MOUNT_POINT"
+        /bin/mount -t ntfs -o rw,uid=1000,gid=1000 "$DEVICE" "$MOUNT_POINT"
         ;;
     *)
-        /usr/bin/mount "$DEVICE" "$MOUNT_POINT"
+        /bin/mount -o rw,uid=1000,gid=1000 "$DEVICE" "$MOUNT_POINT"
         ;;
 esac
 
 MOUNT_STATUS=$?
 if [ $MOUNT_STATUS -eq 0 ]; then
     log_message "Successfully mounted $DEVICE at $MOUNT_POINT"
+    # Set permissions after successful mount
+    /usr/bin/chown -R pi:pi "$MOUNT_POINT"
 else
     log_message "Failed to mount $DEVICE. Exit code: $MOUNT_STATUS"
 fi
